87a38ebfb3638f4096fce8956dcab8067aab1d7a
Zaynab <zayalokova@mail.ru>
2018-04-07 19:20:47 +0300

    lesson 9

--- les8/js/script.js
+++ les8/js/script.js
@@ -1,61 +1,58 @@
  /*jshint esversion: 6 */
  window.addEventListener('DOMContentLoaded',function(){
 
-  	let tab = document.getElementsByClassName('info-header-tab'),
-				tabContent = document.getElementsByClassName('info-tabcontent'),
-				info = document.getElementsByClassName('info-header')[0];
+    let tab = document.getElementsByClassName('info-header-tab'),
+        tabContent = document.getElementsByClassName('info-tabcontent'),
+        info = document.getElementsByClassName('info-header')[0];
 
-			function hideTabContent(a){
-				for (let i = a; i < tabContent.length; i++){
-					tabContent[i].classList.remove('show');
-					tabContent[i].classList.add('hide');
+      function hideTabContent(a){
+        for (let i = a; i < tabContent.length; i++){
+          tabContent[i].classList.remove('show');
+          tabContent[i].classList.add('hide');
 
-				}
-			}
+        }
+      }
 
-			hideTabContent(1);
+      hideTabContent(1);
 
 
    function showTabContent (b) {
-   	if (tabContent[b].classList.contains('hide')) {
-   		hideTabContent(0);
-   		tabContent[b].classList.remove('hide');
-   		tabContent[b].classList.add('show');
-   	}
+    if (tabContent[b].classList.contains('hide')) {
+      hideTabContent(0);
+      tabContent[b].classList.remove('hide');
+      tabContent[b].classList.add('show');
+    }
    }
 
    info.addEventListener('click', function(event){
-   	let target = event.target;
-   	if (target.className == 'info-header-tab') {
-   		for (let i = 0; i < tab.length; i++) {
-   			if (target == tab[i]) {
-   				showTabContent(i);
-   				break;
-   			} 
-   		}
-   	}
+    let target = event.target;
+    if (target.className == 'info-header-tab') {
+      for (let i = 0; i < tab.length; i++) {
+        if (target == tab[i]) {
+          showTabContent(i);
+          break;
+        } 
+      }
+    }
    });
    
 
-
-   let deadline = '2018-03-20';
+let deadline = '2018-04-08';
 
     function getTimeRemaining(endtime) {
          let t = Date.parse(endtime) - Date.parse(new Date()),
          seconds = Math.floor( (t/1000) % 60 ),
          minutes = Math.floor( (t/1000/60) % 60),
          hours = Math.floor( (t/(1000*60*60)) );
-
          return {
             'total': t,
             'hours': hours,
             'minutes': minutes,
             'seconds': seconds
          };
-    };
+    }
 
-   function setClock(id, endtime) {
-
+    function setClock(id, endtime) {
       let timer = document.getElementById(id),
           hours = timer.querySelector('.hours'),
           minutes = timer.querySelector('.minutes'),
@@ -70,15 +67,11 @@
             if (t.total <= 0) {
                clearInterval(timeInterval);
             }
-
-          };
-
+          }
           updateClock();
          let timeInterval = setInterval(updateClock, 1000);
-   };
-
+   }
    setClock('timer', deadline);
-
  });
 
 
